313ffe352644915d774caeb89dd3978c784c505e
K4rleak <karel@frankovi.cz>
2024-05-24 16:05:47 +0200

    24.05

--- src/Components/Publication/PublicationAuthorEditCard.jsx
+++ src/Components/Publication/PublicationAuthorEditCard.jsx
@@ -1,10 +1,69 @@
-import { CardCapsule, DeleteButton, useDispatch } from '@hrbolek/uoisfrontend-shared/src'
-
-//import { GroupMediumCard } from './GroupMediumCard'
+// import { CardCapsule, DeleteButton, useDispatch,SearchInput,CreateAsyncQueryValidator } from '@hrbolek/uoisfrontend-shared/src'
+import { CardCapsule, DeleteButton, Dialog, useDispatch, CreateAsyncQueryValidator, SelectInput, SearchInput} from '@hrbolek/uoisfrontend-shared/src'
 import { PublicationLink } from './PublicationLink'
 import { InsertPublicationAuthorAsyncAction } from '../../Queries/InsertPublicationAuthorAsyncAction'
 import { DeletePublicationAuthorAsyncAction } from '../../Queries/DeletePublicationAuthorAsyncAction'
 import { FetchPublicationByIdAsyncAction } from '../../Queries/FetchPublicationByIdAsyncAction'
+import { useCallback, useState } from 'react'
+import { FetchSearchUserAsyncAction } from '../../Queries/FetchSearchUserAsyncAction'
+import { TextInput } from '@hrbolek/uoisfrontend-shared/src'
+
+const AddAuthorDialog = ({onCreate}) => {
+    const [visible, setVisible] = useState(false)
+    const [data, setData] = useState({
+        roletype_id: "233217fd-e23f-46af-a005-f1a125d39c6a",
+        startdate: new Date().toISOString().split('T')[0],
+        enddate: new Date().toISOString().split('T')[0],
+    })
+    const onOk = () => {
+        setVisible(false)
+        onCreate({...data})
+    }
+
+    const onCancel = () => {
+        setVisible(false)
+    }
+    const onOpen = () => {
+        setVisible(true)
+    }
+    const onChange = useCallback((atributeName) => (value) => {
+        setData(oldData => {
+            const newData =  {...oldData}
+            newData[atributeName] = value
+            console.log(newData)
+            return newData
+        })
+    }, [setData])
+
+    if (visible) {
+        return (
+            <Dialog title="Výběr autora" onOk={onOk} onCancel={onCancel}>
+                {/* <div className="form-floating">
+                    <SelectInput FetchAsyncAction={FetchRoleTypesAsyncAction} id="select" value={data.roletype_id} onChange={onChange("roletype_id")} />
+                    <label htmlFor={"select"}>Typ role</label>
+                </div>                 */}
+                <div className="form-floating">
+                    <TextInput type={"text"} id={"order"} value={0} onChange={onChange("startdate")} />
+                    <label htmlFor={"startdate"}>startdate</label>
+                </div>
+                {/* <div className="form-floating">
+                    <TextInput type={"date"} id={"enddate"} value={data.enddate} onChange={onChange("enddate")} />
+                    <label htmlFor={"enddate"}>enddate</label>
+                </div> */}
+                <SearchInput title="Výběr uživatele" onSelect={onChange("user_id")} FetchByPatternAsyncAction={FetchSearchUserAsyncAction} />
+            </Dialog>
+        )
+    } else {
+        return (
+            <button className='btn btn-success form-control' onClick={onOpen}>+</button>
+        )
+    }
+}
+
+
+
+
+
 
 
 const AuthorRow = ({index, author,publication}) => {
@@ -29,23 +88,33 @@ const AuthorRow = ({index, author,publication}) => {
     )
 }
 
+
 export const PublicationAuthorEditCard = ({publication, filterFunc=(p)=>true}) => {
     const dispatch=useDispatch()
     const authors = publication?.authors || []
     const filtered = authors.filter(filterFunc)
+
+    const onCreate = (data) => {
+        const [onResolve, onReject] = validator(dispatch)
+        const fullRecord = {...data, publication_id: publication.id, id: crypto.randomUUID()}
+        console.log("fullRecord", fullRecord)
+        dispatch(
+            InsertPublicationAuthorAsyncAction(fullRecord)
+        ).then(onResolve, onReject)
+        .then(() => {
+            // dispatch(GroupAsyncActions.read({id: group.id}))
+            dispatch(FetchPublicationByIdAsyncAction(publication))
+        })
+    }
+
     const onClick=()=>{
         const updater = async () => {
-            const variables={id: "22222222-2222-2222-2222-222222222222", user_id: "89d1f48a-ae0f-11ed-9bd8-0242ac110002", publication_id: "cb3c3978-e716-46ac-9a3b-bb8f9d806a46", share: 15, order: 2}
+            const variables={id: crypto.randomUUID(), user_id: "89d1f48a-ae0f-11ed-9bd8-0242ac110002", publication_id: "cb3c3978-e716-46ac-9a3b-bb8f9d806a46", share: 15, order: 2}
             await dispatch(InsertPublicationAuthorAsyncAction(variables))
             await dispatch(FetchPublicationByIdAsyncAction(publication))
         }
         updater()
     }
-    // const onClick=()=>{
-    //     const variables={id: "22222222-2222-2222-2222-222222222222", user_id: "89d1f48a-ae0f-11ed-9bd8-0242ac110002", publication_id: "cb3c3978-e716-46ac-9a3b-bb8f9d806a46", share: 15, order: 2}
-    //     dispatch(InsertPublicationAuthorAsyncAction(variables))
-
-    // }
     return (
         <CardCapsule title={<>Autoři <PublicationLink publication={publication} /></>}>
             <table className='table table-striped table-bordered'>
@@ -63,8 +132,8 @@ export const PublicationAuthorEditCard = ({publication, filterFunc=(p)=>true}) =
                         (a, i) => <AuthorRow index={i+1} key={a.id} author={a} publication={publication} />
                     )}
                     <tr>
-                        <td colSpan={5}><button className='btn btn-success form-control' onClick={onClick}>+</button></td>
-                        {/* <td colSpan={5}><button className='btn btn-success form-control'>+</button></td> */}
+                        {/* <td colSpan={5}><button className='btn btn-success form-control' onClick={onClick}>+</button></td> */}
+                        <td colSpan={5}><AddAuthorDialog publication={publication} onCreate={onCreate}/></td>
                     </tr>
                 </tbody>
             </table>
