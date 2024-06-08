6341792d72e62c12c972bd9acb87f3f37ff07c9e
K4rleak <49683415+K4rleak@users.noreply.github.com>
2024-06-08 15:07:29 +0200

    Add files via upload

--- src/Pages/AuthorEditPage.jsx
+++ src/Pages/AuthorEditPage.jsx
@@ -1,26 +1,22 @@
 // import { useFreshItem } from "@hrbolek/uoisfrontend-shared/src"
 import { useParams } from "react-router-dom"
-import { UserLargeCard } from "../Components"
 import { useFreshItem, CreateAsyncQueryValidator, useDispatch } from "@hrbolek/uoisfrontend-shared/src"
-import { FetchUserByIdAsyncAction } from "../Queries"
-import { FetchEventByIdAsyncAction } from "../Queries/FetchEventByIdAsyncAction"
-import { EventLargeCard } from "../Components/Event/EventLargeCard"
-import { FetchPublicationByIdAsyncAction } from "../Queries/FetchPublicationByIdAsyncAction"
 
-const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst uživatele", success: "Načtení uživatele se povedlo"})
+import { FetchPublicationByIdAsyncAction } from "../Queries/FetchPublicationByIdAsyncAction"
+import { PublicationLargeCard } from "../Components/Publication/PublicationLargeCard"
+import { PublicationEditCard } from "../Components/Publication/PublicationEditCard"
+import { PublicationAuthorEditCard } from "../Components/Publication/PublicationAuthorEditCard"
+
+const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst publikaci", success: "Načtení publikace se povedlo"})
 export const AuthorEditPage = ()  => {
-    const {publication_id} = useParams()
+    const {id} = useParams()
     const [onResolve, onReject] = validator(useDispatch())
-    const [publication, userPromise] = useFreshItem({publication_id}, FetchPublicationByIdAsyncAction)
+    const [publication, userPromise] = useFreshItem({id}, FetchPublicationByIdAsyncAction)
     userPromise.then(onResolve, onReject)
 
     if (publication) {
         return (
-            // <UserLargeCard user={user} />
-            // <div>Události nahrány
-            //     {JSON.stringify(event)}
-            // </div>
-            <PublicationAuthorCard publication={publication} />
+                <PublicationAuthorEditCard publication={publication} />
         )
     } else {
         return (
